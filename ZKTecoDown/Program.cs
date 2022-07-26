06cba70d7e0d238f3972ded3411d1afa05fa1d01
gullinoagus <agullino@itba.edu.ar>
2022-07-25 21:44:13 -0300

    Ready for testing

--- ZKTecoDown/Program.cs
+++ ZKTecoDown/Program.cs
@@ -8,10 +8,34 @@ namespace ZKTecoDown
         [STAThread]
         static void Main()
         {
+            //Block multiple instances from runing
+            bool isAnotherInstanceOpen;
+            using(var mutex = new Mutex(true, "ZKTecoDownloader", out isAnotherInstanceOpen))
+            {
+                if (!isAnotherInstanceOpen)
+                {
+                    MessageBox.Show("El programa ya se esta ejecutando, revise la bandeja de aplicaciones.",
+                        "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+            
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new MachineDL());
+                try
+                {
+                    ApplicationConfiguration.Initialize();
+                    Application.Run(new MachineDL());
+
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Error inesperado contacte a un tecnico.\n" +
+                        e.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+                mutex.ReleaseMutex();
+            }
+
         }
     }
 }
